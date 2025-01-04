d33707d3bfcb87a43ba5ba1b4709e7421a5e0aa5
Frank Gallo <gallofrank806@gmail.com>
2025-01-04 13:37:42 -0500

    refactor(main) : os PORT

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 from src.routers.book_router import book_router
 from src.routers.securizacion_jwt_router import booksecurity
+import os
 
 app = FastAPI()
 
 @app.get("/", tags=["Home"])
 def index():
-    return {"Welcome":"API books"}
+    return {"Welcome": "API books"}
 
 app.include_router(router=booksecurity)
-app.include_router(prefix='/book',router=book_router)
+app.include_router(prefix="/book", router=book_router)
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
